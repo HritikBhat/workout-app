9c7c1b21a9febc85ee3884ce785ed482163e2748
GAURAV <Gaurav1+@>
2020-11-17 19:18:14 +0530

    Workout App

--- app/src/main/java/com/example/workoutapp/excersisefrag.java
+++ app/src/main/java/com/example/workoutapp/excersisefrag.java
@@ -1,5 +1,6 @@
 package com.example.workoutapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,34 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class excersisefrag extends Fragment {
+    int[] newArray;
+
     View v;
 
     public excersisefrag(){
 
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        newArray=new int[]{
+                R.id.chest,R.id.backbody,R.id.abs,R.id.lowerpart
+        };
+
+    }
+    public void ImageButtonClicked(View view){
+        int i=0;
+        for(i=0;i<newArray.length;i++){
+            int value =i+1;
+            Intent intent=new Intent(getActivity().getApplication(), secexcersise.class);
+            intent.putExtra("value",String.valueOf(value));
+            startActivity(intent);
+
+        }
+    }
+
 
     @Nullable
     @Override
